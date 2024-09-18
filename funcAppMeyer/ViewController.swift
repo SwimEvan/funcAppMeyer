a49a93795e01efc6756a8f6c5ef3293e60282a5b
Evan Meyer <evan.meyer26@d155.org>
2024-09-18 07:57:55 -0500

    Initial Commit

---
+++ funcAppMeyer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  funcAppMeyer
+//
+//  Created by EVAN MEYER on 9/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ funcAppMeyerTests/funcAppMeyerTests.swift
@@ -0,0 +1,17 @@
+//
+//  funcAppMeyerTests.swift
+//  funcAppMeyerTests
+//
+//  Created by EVAN MEYER on 9/18/24.
+//
+
+import Testing
+@testable import funcAppMeyer
+
+struct funcAppMeyerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
